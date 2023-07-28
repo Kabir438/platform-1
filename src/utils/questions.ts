fd3b70b3aefdf7c52e35688115170e98bcbbba08
Kabir Chawla <kabir.438.2025@doonschool.com>
2023-07-28 20:32:19 +0530

    fixes

--- src/utils/questions.ts
+++ src/utils/questions.ts
@@ -1,45 +1,49 @@
 const questions = [
   {
-    question: `The riddle:
+    question: `The riddle: 
       Count the carbons, in a chain they align, Six in number, a clue intertwined. Hydrogens join, their count you must seek, Multiplying onwards, the answer you'll peek.Oxygen whispers, its presence made known, A vital ingredient, in our body it's shown.For in the realm of riddles, knowledge shall rise, Revealing the mass, where the answer lies.
     `,
     answers: [
         "180"
     ]
   },
+  
   {
     question: `
-    Crack the code, the secret untold. In numbers and symbols, the message unfolds. Seek the key that unlocks the way, and decipher the puzzle's display
+    "Crack the code, the secret untold. In numbers and symbols, the message unfolds. Seek the key that unlocks the way, and decipher the puzzle's display"
 
     7325- 2 right and in the incorrect place.
     9146- 2 right but is in the incorrect place.
     5201- 2 right and in the right position
-    3680- all in the wrong places.
+    3680- all are wrong
     `,
     answers: [
         "5249"
     ]
   },
+
   {
     question: "From the beginning of the beryl's gleam, to the end of fiery dance. Seek the lightest of metals, a foundation to be, and the heaviest alkali that sets atoms free. Use our combination to unlock what it is you seek.",
     answers: [
         "487"
     ]
   },
+
   {
-    question: "The clue- In ones and zeroes, the answer is true. A binary sequence holds the key, To a number that sets you free. Digits align, binary's embrace, Unveils the secret, at a steady pace.In the sequence 1000000000 lies the number that you embrace.",
+    question: `
+      The clue: In ones and zeroes, the answer is true. A binary sequence holds the key, To a number that sets you free. Digits align, binary's embrace, Unveils the secret, at a steady pace.In the sequence 1000000000 lies the number that you embrace.
+    `,
     answers: [
         "512"
     ]
   },
   {
-    question: `The team will use their answer’s till here to produce a phone number - 180, 3719, 487, 512. They will remove the smallest number from each of these numbers and add a 0 at the end to get the number. They will be told to figure out which number they have to call back on earth.
-    
-    \"In numbers we dwell, a riddle to tell,
-    Four numbers and digits, a puzzle to excel.
-    Seek the smallest among each, it must be expelled,
-    Combine what's left and add something but nothing at the end.
-    Then the solution will align.\"
+    question: `
+      Unlock the secrets of the past, for the answer lies in the numbers amassed. Remember the quartet you've once possessed, together they'll lead you to the treasure chest. In numbers we dwell, a riddle to tell,
+      Four numbers and digits, a puzzle to excel.
+      Seek the smallest among each, it must be expelled,
+      Combine what's left and add something but nothing at the end.
+      Then the solution will align.   
     `,
     answers: [
         "1803719487512"
